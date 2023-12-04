686a3143c1d3d2b758675831d51d2c533ba13618
haein-side <kimhi1640@gmail.com>
2023-12-04 19:04:34 +0900

    Chore: 웹 서버 생성 및 Phase 분리

---
+++ src/test/java/com/haein/hanghae/HanghaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haein.hanghae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
